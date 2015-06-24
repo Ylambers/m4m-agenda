e414dd37e5d17fc0ce1b6c287d8b223ffff02474
Alwin <alwinkroesen@gmail.com>
2015-06-24 11:04:06 +0200

    reservations

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -17,21 +17,51 @@ use AppBundle\Entity\Room;
 class AppController extends Controller
 {
     /**
-     * @Route("/app", name="appAPI")
+     * @Route("/app/{type}", defaults={"type" = "rooms"}, name="appAPI")
      */
-    public function indexAction()
+    public function indexAction($type)
     {
-        $bookings = $this->getDoctrine()
-            ->getRepository('AppBundle:Room')
-            ->findAll();
+        if($type == "rooms"){
 
-        $response = new Response();
-        $page = array();
-        foreach($bookings as $booking){
-            $page[$booking->getId()] = array();
-            $page[$booking->getId()]['name'] = $booking->getName();
-            $page[$booking->getId()]['seats'] = $booking->getSeats();
+            $bookings = $this->getDoctrine()
+                ->getRepository('AppBundle:Room')
+                ->findAll();
+
+            $response = new Response();
+            $page = array();
+            foreach($bookings as $booking){
+                $page[$booking->getId()] = array();
+                $page[$booking->getId()]['name'] = $booking->getName();
+                $page[$booking->getId()]['seats'] = $booking->getSeats();
+            }
+
+        }elseif($type == "reservations"){
+
+            $bookings = $this->getDoctrine()
+                ->getRepository('AppBundle:Applicant')
+                ->findAll();
+
+            $response = new Response();
+            $page = array();
+            foreach($bookings as $booking){
+                $page[$booking->getId()] = array();
+                $page[$booking->getId()]['name'] = $booking->getName();
+                $page[$booking->getId()]['lastname'] = $booking->getLastName();
+                $page[$booking->getId()]['date'] = $booking->getDate();
+                $page[$booking->getId()]['startTime'] = $booking->getTimeStart();
+                $page[$booking->getId()]['endTime'] = $booking->getTimeEnd();
+                $page[$booking->getId()]['participants'] = $booking->getParticipants();
+                $page[$booking->getId()]['reason'] = $booking->getReason();
+                $page[$booking->getId()]['room'] = array();
+                $page[$booking->getId()]['room']['id'] = $booking->getRoom()->getId();
+                $page[$booking->getId()]['room']['name'] = $booking->getRoom()->getName();
+                $page[$booking->getId()]['room']['seats'] = $booking->getRoom()->getSeats();
+            }
+        }elseif($type == "reservation"){
+
         }
+
+
         $response->setContent(json_encode($page));
         $response->headers->set('Content-Type', 'application/json');
         return $response;
