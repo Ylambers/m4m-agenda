afc569000684247d7f9cab85ec101021a9629ebe
Alwin <alwinkroesen@gmail.com>
2015-06-25 12:07:24 +0200

    Time check fixed

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -19,13 +19,17 @@ use AppBundle\Entity\Applicant;
 
 class BookingController extends Controller
 {
+
+    private $text = array();
+
     /**
      * @Route("/booking", name="Booking")
      */
     public function Booking(request $request)
     {
-        $message = array();
         $booking = new Applicant();
+        $this->text['error'] = array();
+
 
         $formBooking = $this->createFormBuilder($booking)
 
@@ -79,27 +83,82 @@ class BookingController extends Controller
 
         if ($formBooking->isValid()){
             $em = $this->getDoctrine()->getManager();
-            $bookEntity = $em->getRepository("AppBundle:Applicant")->findBy(array("date" => $booking->getDate()));
-            $time_start = $em->getRepository("AppBundle:Applicant")->findBy(array("timeStart"=>$formBooking->get("timeStart") -> getViewData()));
-            $time_end = $em->getRepository("AppBundle:Applicant")->findBy(array("timeEnd"=>$formBooking->get("timeEnd") -> getViewData()));
 
-            if (count($bookEntity) == 0) {
-                if (count($time_start) == 0){
-                    $em = $this->getDoctrine()->getManager();
-                    $booking->setRoom($formBooking->get("room")->getData());
-                    $em->persist($booking);
-                    $em->flush();
+            $booking->setRoom($formBooking->get("room")->getData());
+
+
+            $reservations = $em->getRepository("AppBundle:Applicant")->findBy(array("date" => $booking->getDate(),"room" => $booking->getRoom()));
+            $error = 0;
+            foreach($reservations as $reservation){
+                $timeStart = $reservation->getTimeStart()->format('H:i:s');
+                $timeEnd = $reservation->getTimeEnd()->format('H:i:s');
+                $bookingTimeStart = $booking->getTimeStart()->format('H:i:s');
+                $bookingTimeEnd = $booking->getTimeEnd()->format('H:i:s');
+//                echo $bookingTimeStart ." > ".$timeStart." && ". $bookingTimeStart." < ".$timeEnd;
+//                echo "<br />";
+//                echo $bookingTimeEnd ." > ".$timeStart." && ". $bookingTimeEnd." < ".$timeEnd;
+//                echo "<br />";
+//                echo $bookingTimeStart ." > ".$timeStart." && ". $bookingTimeEnd." < ".$timeEnd;
+//                echo '<br /><br /><br />';
+
+                if($bookingTimeStart >= $timeStart && $bookingTimeStart <= $timeEnd){
+                    $error++;
                 }
-            }elseif(count($bookEntity) == 1){
-                // return new Response ('Er is al iets gepland');
-                $errorMsg = "Er is al iets gepland";
+                if($bookingTimeEnd >= $timeStart && $bookingTimeEnd <= $timeEnd){
+                    $error++;
+                }
+                if($bookingTimeStart <= $timeStart && $bookingTimeEnd >= $timeEnd){
+                    $error++;
+                }
+
             }
+            if($error > 0){
+                $this->text['error'][] = "Deze ruimte is al op deze tijd bezet.";
+            }
+            if($booking->getTimeEnd() < $booking->getTimeStart()){
+                $this->text['error'][] = "De eind tijd kan niet minder zijn als de start tijd.";
+                $error++;
+            }
+
+            if($error == 0){
+                $em->persist($booking);
+                $em->flush();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //$time_start = $em->getRepository("AppBundle:Applicant")->findBy(array("timeStart"=>$formBooking->get("timeStart") -> getViewData()));
+            //$time_end = $em->getRepository("AppBundle:Applicant")->findBy(array("timeEnd"=>$formBooking->get("timeEnd") -> getViewData()));
+//
+//            if (count($bookEntity) == 0) {
+//                if (count($time_start) == 0){
+//                    $em = $this->getDoctrine()->getManager();
+//                    $booking->setRoom($formBooking->get("room")->getData());
+//                    $em->persist($booking);
+//                    $em->flush();
+//                }
+//            }elseif(count($bookEntity) == 1){
+//                // return new Response ('Er is al iets gepland');
+//                $errorMsg = "Er is al iets gepland";
+//            }
         }
 
 
     return $this->render('default/book.html.twig',
         array(
             'formBooking' => $formBooking->createView(),
+            'texts' => $this->text,
         ));
     }
 }
