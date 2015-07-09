f96406cbb672161cfaaf4a031f438ec4ecc0e15a
Ylambers <y.lambers@outlook.com>
2015-07-09 17:01:14 +0200

    Tokens change by click

--- web/js/Index.js
+++ web/js/Index.js
@@ -2,3 +2,11 @@
  * Created by yaron on 1-7-2015.
  */
 
+function errorMsg(){
+    var responseText = document.querySelector("#responseText");
+    responseText.innerHTML = "";
+
+    responseText.setAttribute("class", "display:none;");
+}
+
+errorMsg();
